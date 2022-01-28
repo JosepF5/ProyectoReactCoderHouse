ca4901b3732c605bc0f6a31e8ee2622a5fad3661
Josep Palomino <jfpalomino.22@gmail.com>
2022-01-27 20:07:20 -0500

    Componente_V1

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ItemListContainer(){
+    return(
+        <>
+            <div>
+                Hola Soy Item List Container
+            </div>
+        </>
+    )
+}
+
+export default ItemListContainer
